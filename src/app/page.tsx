53c7aa9876b4090da199e864ff7f854bd2b89f6f
Rimsha Aslam <rimsha.aslam.230@gmail.com>
2024-12-17 22:00:22 +0500

    Your commit message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ export default async function Home() {
   const url = await fetch("https://simple-books-api.glitch.me/books");
   const response = await url.json();
   console.log(response);
+
   return (
     <main>
       <h1 className="text-6xl text-emerald-800 ">Books Name</h1>
       {response.map((book: any) => (
-        <div>
+        <div key={book.id}> {/* Use a unique key for each element */}
           <Link href={`${book.type}`}>
             <h1 className="text-5xl text-lime-500 hover:text-cyan-800">{book.name}</h1>
           </Link>
